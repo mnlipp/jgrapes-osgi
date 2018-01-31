6ea858ebfc40186d9d93b6b913e0aeb37733a805
Michael N. Lipp <mnl@mnl.de>
2018-01-31 23:17:47 +0100

    Changed port.

--- org.jgrapes.osgi.demo.portal/src/org/jgrapes/osgi/demo/portal/Application.java
+++ org.jgrapes.osgi.demo.portal/src/org/jgrapes/osgi/demo/portal/Application.java
@@ -88,7 +88,7 @@ public class Application extends Component implements BundleActivator {
 		sslContext.init(kmf.getKeyManagers(), null, new SecureRandom());
 		// Create a TCP server for SSL
 		Channel securedNetwork = app.attach(
-				new TcpServer().setServerAddress(new InetSocketAddress(5443))
+				new TcpServer().setServerAddress(new InetSocketAddress(6443))
 				.setBacklog(3000).setConnectionLimiter(new PermitsPool(50)));
 		// Network level unencrypted channel.
 		Channel httpTransport = new NamedChannel("httpTransport");
