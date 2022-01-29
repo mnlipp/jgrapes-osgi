94dcf794dcea2c126608735c4072f0fe8f6936b1
Michael N. Lipp <mnl@mnl.de>
2022-01-29 17:43:21 +0100

    Re-implement using ComponentProvider.

--- org.jgrapes.osgi.core/src/org/jgrapes/osgi/core/ComponentCollector.java
+++ org.jgrapes.osgi.core/src/org/jgrapes/osgi/core/ComponentCollector.java
@@ -1,6 +1,6 @@
 /*
  * JGrapes Event Driven Framework
- * Copyright (C) 2016, 2018  Michael N. Lipp
+ * Copyright (C) 2016, 2022  Michael N. Lipp
  *
  * This program is free software; you can redistribute it and/or modify it 
  * under the terms of the GNU Affero General Public License as published by 
@@ -18,21 +18,19 @@
 
 package org.jgrapes.osgi.core;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.StreamSupport;
+import static java.util.function.Predicate.not;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.jgrapes.core.Channel;
-import org.jgrapes.core.Component;
 import org.jgrapes.core.ComponentFactory;
-import org.jgrapes.core.ComponentType;
-import org.jgrapes.core.Components;
-import org.jgrapes.core.Manager;
 import org.jgrapes.core.events.Stop;
+import org.jgrapes.util.ComponentProvider;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
@@ -47,10 +45,20 @@ import org.osgi.util.tracker.ServiceTrackerCustomizer;
  * Effectively, the component collector leverages OSGi's service layer
  * to modify the component tree at run-time.
  * 
+ * This class uses {@link ComponentProvider#setFactories(ComponentFactory...)} 
+ * and {@link ComponentProvider#setPinned(List)} for its implementation.
+ * As it inherits from {@link ComponentProvider}, it automatically
+ * supports the provisioning of additional components through
+ * {@link ConfigurationUpdate} events. If this is not desired, invoke
+ * {@link ComponentProvider#setComponentsEntry(String)} with `null` as
+ * argument. 
+ * 
  * @param <F> the component factory type
  */
-public class ComponentCollector<F extends ComponentFactory> extends Component
-        implements ServiceTrackerCustomizer<F, F> {
+@SuppressWarnings({ "PMD.DataflowAnomalyAnalysis",
+    "PMD.DataflowAnomalyAnalysis" })
+public class ComponentCollector<F extends ComponentFactory>
+        extends ComponentProvider implements ServiceTrackerCustomizer<F, F> {
 
     private static final List<Map<Object, Object>> SINGLE_DEFAULT
         = Arrays.asList(Collections.emptyMap());
@@ -58,7 +66,7 @@ public class ComponentCollector<F extends ComponentFactory> extends Component
     private BundleContext context;
     @SuppressWarnings("PMD.SingularField")
     private ServiceTracker<F, F> serviceTracker;
-    private Function<String, List<Map<Object, Object>>> matcher;
+    private Function<String, List<Map<Object, Object>>> configurator;
 
     /**
      * Creates a collector component that uses a {@link ServiceTracker} 
@@ -70,15 +78,15 @@ public class ComponentCollector<F extends ComponentFactory> extends Component
      * @param componentChannel this component's channel
      * @param context the OSGi {@link BundleContext}
      * @param factoryCls the factory class
-     * @param matcher the matcher function
+     * @param configurator the function that provides the pinned configurations
      */
     public ComponentCollector(
             Channel componentChannel, BundleContext context,
             Class<F> factoryCls,
-            Function<String, List<Map<Object, Object>>> matcher) {
+            Function<String, List<Map<Object, Object>>> configurator) {
         super(componentChannel);
         this.context = context;
-        this.matcher = matcher;
+        this.configurator = configurator;
         serviceTracker = new ServiceTracker<>(context, factoryCls, this);
         serviceTracker.open();
     }
@@ -97,37 +105,52 @@ public class ComponentCollector<F extends ComponentFactory> extends Component
     }
 
     /**
-     * Whenever a new factory is added, it is used to create component instances
-     * with this component's channel. First, the `matcher` function passed to
-     * the constructor is invoked with the name of the class of the component to
-     * be created as argument. The list of maps returned is used to create
-     * components, passing each element in the list as parameter to
-     * {@link ComponentFactory#create(Channel, Map)}. The map return from the
-     * `matcher` is automatically augmented with an entry with key
-     * {@link BundleContext}.class and the bundle context passed to the
-     * constructor.
+     * Whenever a new factory is added, it is used to create component 
+     * instances with this component's channel. First, the `configurator`
+     * passed to the constructor is invoked with the name of the class 
+     * of the component to be created as argument. The list of maps 
+     * returned is then added to the pinned components 
+     * (see {@link ComponentProvider#setPinned(List)}). 
+     * 
+     * The map return from the `configurator` is automatically augmented
+     * with an entry with key {@link BundleContext}.class and the bundle 
+     * context passed to the constructor.
      *
      * @see ServiceTrackerCustomizer#addingService(ServiceReference)
      */
     @Override
     public F addingService(ServiceReference<F> reference) {
         F factory = context.getService(reference);
-        // Iterate over all nodes of the subtree that has this
-        // component as root.
-        if (StreamSupport.stream(spliterator(), false)
-            .filter(cls -> cls.getClass().equals(factory.componentType()))
-            .count() == 0) {
-            List<Map<Object, Object>> configs = matcher.apply(
-                factory.componentType().getName());
-            for (Map<?, ?> config : configs) {
-                @SuppressWarnings({ "PMD.AvoidInstantiatingObjectsInLoops",
-                    "PMD.UseConcurrentHashMap" })
-                Map<Object, Object> props = new HashMap<>(config);
-                props.put(BundleContext.class, context);
-                factory.create(channel(), props).ifPresent(
-                    component -> attach(component));
-            }
+        if (factories().containsKey(factory.componentType().getName())) {
+            // Factory for the new component type is already known.
+            return factory;
         }
+
+        // Add configuration to provider before adding new factory
+        List<Map<?, ?>> newConfigs = Stream.concat(
+            // filter existing configs to avoid duplicates
+            pinned().stream().filter(not(c -> factory.componentType().getName()
+                .equals(c.get(COMPONENT_TYPE)))),
+            // add new configs
+            configurator.apply(
+                factory.componentType().getName()).stream().map(c -> {
+                    // Received may be immutable
+                    var newMap = new HashMap<>(c);
+                    newMap.put(BundleContext.class, context);
+                    if (!c.containsKey(COMPONENT_TYPE)) {
+                        newMap.put(COMPONENT_TYPE,
+                            factory.componentType().getName());
+                    }
+                    return newMap;
+                }))
+            .collect(Collectors.toList());
+        setPinned(newConfigs);
+
+        // Now add factory
+        ComponentFactory[] updatedFactories
+            = Stream.concat(factories().values().stream(), Stream.of(factory))
+                .toArray(s -> new ComponentFactory[s]);
+        setFactories(updatedFactories);
         return factory;
     }
 
@@ -142,8 +165,8 @@ public class ComponentCollector<F extends ComponentFactory> extends Component
     }
 
     /**
-     * Removes all child component with the type produced by the factory that is
-     * removed. A (possibly) final {@link Stop} event is send to the
+     * Deletes all child components with the type produced by the factory that 
+     * is removed. A (possibly) final {@link Stop} event is send to the
      * detached subtrees which may be used to deactivate bundles.
      * 
      * @see ServiceTrackerCustomizer#removedService(ServiceReference, Object)
@@ -151,17 +174,18 @@ public class ComponentCollector<F extends ComponentFactory> extends Component
     @Override
     @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
     public void removedService(ServiceReference<F> reference, F service) {
-        // Avoid ConcurrentModificationException
-        List<ComponentType> toBeRemoved = new ArrayList<>();
-        for (ComponentType child : this) {
-            if (child.getClass().equals(service.componentType())) {
-                toBeRemoved.add(child);
-            }
-        }
-        for (ComponentType item : toBeRemoved) {
-            Manager mgr = Components.manager(item);
-            mgr.detach();
-            mgr.activeEventPipeline().fire(new Stop());
-        }
+        // Remove factory.
+        ComponentFactory[] updatedFactories = factories().values().stream()
+            .filter(f -> !f.componentType().getName()
+                .equals(service.componentType().getName()))
+            .toArray(s -> new ComponentFactory[s]);
+        setFactories(updatedFactories);
+
+        // Remove configuration
+        List<Map<?, ?>> updatedConfigs = pinned().stream()
+            .filter(not(c -> service.componentType().getName()
+                .equals(c.get(COMPONENT_TYPE))))
+            .collect(Collectors.toList());
+        setPinned(updatedConfigs);
     }
 }
